b42a2b2b4e65d5ed3cffdecebf87a017d143267f
Llohee <nggiang141@gmail.com>
2024-04-10 19:05:02 +0700

    update .env file

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const { userRouter } = require("./routes/user");
 const jwt = require("jsonwebtoken");
 const { users, userModel } = require("./models/user");
 const bcrypt = require("bcrypt");
-// const { songRouter } = require("./routes/song");
 const cors = require("cors");
 
 const app = express();
 
-// mongoose.connect('mongodb://0.0.0.0:27017/mindx')
-mongoose.connect(
-  "mongodb+srv://nggiang141:Agera141@cluster0.ozq4c25.mongodb.net/"
-);
+const url = process.env.MONGO_URL;
+mongoose.connect(url, {
+  autoIndex: true,
+});
+const db = mongoose.connection;
+db.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.use(express.json());
 app.use(cors());
@@ -27,9 +31,7 @@ const authenticationCheck = async (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, "123@lol");
     const { email } = decoded;
-    // Check user co trong co so du lieu khong
     const user = await userModel.findOne({ email: email });
-    // .populate("songs");
     if (user) {
       req.user = user;
       next();
@@ -43,7 +45,6 @@ const authenticationCheck = async (req, res, next) => {
 };
 
 app.use("/users", authenticationCheck, userRouter);
-// app.use("/songs", authenticationCheck, songRouter);
 
 app.get("/", (req, res) => {
   res.send("Run on Vercel");
@@ -51,12 +52,9 @@ app.get("/", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  //check trong db
   const user = await userModel.findOne({ email });
-  //nếu có user thì trả token , còn không thì trả lỗi
   if (user && bcrypt.compareSync(password, user.password)) {
     const accesstoken = jwt.sign({ email: email }, "123@lol");
-    // Tra token cho client
     res.send({ token: accesstoken });
   } else {
     res.send("khong tim thay");
@@ -65,12 +63,7 @@ app.post("/login", async (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
-  // check trùng email trong db
   const existringUser = await userModel.findOne({ email });
-  // nếu trùng thì không cho tạo , nếu không trùng thì tạo user
-  // tim user có usename == req.body.email
-  // nếu tồn tại thì res.send('user da ton tại )
-  // nếu ko thì create
   if (existringUser) {
     res.send("user ton tại");
   } else {
@@ -89,6 +82,6 @@ app.put("/update", async (req, res) => {
   const { email, password } = res.body;
 });
 
-app.listen(8000, ()=> console.log("Run on port 8000"));
+app.listen(8000, () => console.log("Run on port 8000"));
 console.log("Server running");
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,92 +1,79 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const {  userModel } = require('../models/user')
-const multer = require('multer')
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { userModel } = require("../models/user");
+const multer = require("multer");
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: "uploads/" });
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
 const authorizationCheck = (req, res, next) => {
-    const userRoles = req.users.role
-    console.log(userRoles)
-    // Check xem user nay co quyen lay toan bo user khong (Authorization) == check role
-    if (userRoles.includes('admin')) {
-        next()
-    } else {
-        res.send('User khong co quyen')
-    }
+  const userRoles = req.users.role;
+  console.log(userRoles);
+  if (userRoles.includes("admin")) {
+    next();
+  } else {
+    res.send("User khong co quyen");
+  }
+};
 
-}
+userRouter.get("/", authorizationCheck, async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.send(users);
+  } catch (error) {
+    res.send("Error");
+    console.log(error);
+  }
+});
 
-userRouter.get('/', authorizationCheck, async (req, res) => {
-    try {
-        const users = await userModel.find({})
-        res.send(users)
-    } catch (error) {
-        res.send('Error')
-        console.log(error)
-    }
+userRouter.get("/me", (req, res) => {
+  res.send(req.user);
+});
 
-})
+userRouter.get("/", authorizationCheck, async (req, res) => {
+  const users = await userModel.find({});
+  const user = req.user;
+});
 
-userRouter.get('/me', (req, res) => {
-    res.send(req.user)
-})
+//check role user
+userRouter.patch("/:email", authorizationCheck, async (req, res) => {
+  const { role, song } = req.body;
+  const email = req.params.email;
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const user = await userModel.findOneAndUpdate(
+      { email },
+      { $push: { songs: song } },
+      { new: true }
+    );
+    res.send(user);
+  } else {
+    res.send("Khong co user");
+  }
+});
 
+userRouter.post("/create", authorizationCheck, async () => {});
 
-userRouter.get('/', authorizationCheck, async (req, res) => {
-    const users = await userModel.find({})
-    const user = req.user
-})
+userRouter.delete("/:email", authorizationCheck, async (req, res) => {
+  const email = req.params.email;
+  const currentUser = req.user;
+  if (currentUser.email === email) {
+    res.status(400).send("Khong the xoa user nay");
+    return;
+  }
+  const user = await userModel.findOne({ email });
+  if (user) {
+    await userModel.deleteOne({ email });
+    res.send("Da xoa,");
+  } else {
+    res.send("Khong co user");
+  }
+});
 
-// Update role cua user
-userRouter.patch('/:email', authorizationCheck, async (req, res) => {
-    const { role, song } = req.body
-    const email = req.params.email
-    // Tim xem co user khong findOne
-    const user = await userModel.findOne({ email })
-    // Neu co thi xoa
-    if (user) {
-        // Update role cho user nay updateOne
-        // await userModel.updateOne({email}, {role})
-        // const user = await userModel.findOne({email})
-        // const user = await userModel.findOneAndUpdate({email}, {role}, {new: true})
-        const user = await userModel.findOneAndUpdate({ email }, { $push: { songs: song } }, { new: true })
-        // Gui lai user duoc update cho client
-        res.send(user)
-    } else {
-        res.send('Khong co user')
-    }
-})
+userRouter.post("/profile", upload.any("avatar"), (req, res) => {
+  console.log(req.file);
+  res.send("ok");
+});
 
-userRouter.post('/create', authorizationCheck, async () => {
-
-})
-
-userRouter.delete('/:email', authorizationCheck, async (req, res) => {
-    // Lay email tu params
-    const email = req.params.email
-    // Check xem email co phai cua user hien tai khong?
-    const currentUser = req.user
-    if (currentUser.email === email) {
-        res.status(400).send('Khong the xoa user nay')
-        return
-    }
-    // Tim xem user co trong db khong?? userModel.findOne({email})
-    const user = await userModel.findOne({ email })
-    // Neu co thi xoa
-    if (user) {
-        await userModel.deleteOne({ email })
-        res.send('Da xoa,')
-    } else {
-        res.send('Khong co user')
-    }
-})
-
-userRouter.post('/profile', upload.any('avatar'), (req, res) => {
-    console.log(req.file)
-    res.send('ok')
-})
-
-module.exports = { userRouter }
+module.exports = { userRouter };
